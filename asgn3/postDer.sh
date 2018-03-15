1962cb0bd84ddd5cfd6fa4631b84f11df750d543
Amrit Singhal <amritsinghal97@gmail.com>
2018-03-15 21:11:27 +0530

    Complete. Almost...

--- asgn3/postDer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-n=$(wc -l < der1.txt)
-k=1
-echo "$n" 
-while IFS= read -r line; do
-	n=$(echo "$line" | wc -w)
-	m=$(($n-$k))
-		echo "$m $line"
-done < der1.txt
--- asgn3/src/finalScript.sh
+++ asgn3/src/finalScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 bin/asgn3 $1 >test.txt
-echo "start" >> test.txt
+# cat test.txt
 tac test.txt >der.txt
-n=$(wc -l < der1.txt)
+n=$(wc -l < der.txt)
 k=1
 echo "$n" >postD
+echo "1 start" >> postD
 while IFS= read -r line; do
 	n=$(echo "$line" | wc -w)
 	m=$(($n-$k))
@@ -12,5 +13,5 @@ while IFS= read -r line; do
 done < der.txt >>postD
 name=$(echo "$1" | cut -f2 -d"/" | cut -f1 -d".")
 name="$name.html"
-./bin/toHTML < postD > name
-rm der.txt test1.txt postD
+bin/toHTML < postD >"$name"
+rm der.txt test.txt postD
