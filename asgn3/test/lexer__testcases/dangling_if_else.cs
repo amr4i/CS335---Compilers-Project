5ff6ae13bafd74d17cc228fffcdbdef723717ff7
Siddharth Agrawal <sidarth@iitk.ac.in>
2018-03-31 17:05:52 +0530

    .

---
+++ asgn3/test/lexer__testcases/dangling_if_else.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Test
+{
+	public void Main()
+	{
+		    int i = 0;
+		    int a=9;
+		    if (i<=3)
+		        a=i+3;
+		    if (i>=2)
+		        a=i-2;
+		    else 
+		        a=a+1;
+
+	}
+}
