c3eb8bbb98d83bc8f7b565472ee6020f577be569
Amrit Singhal <amritsinghal97@gmail.com>
2018-04-01 21:39:15 +0530

    gen2OpCode function done for + and -

--- asgn4/src/genTAC.cpp
+++ asgn4/src/genTAC.cpp
@@ -18,37 +18,73 @@ struct genNode{
 			tac->opType = 3;
 			tac->dest = temp;
 			if(s1->isLit && s2->isLit){
-				if(s1->type=="int" && s2->type=="int"){
-					tac->isInt1 = true;
-					tac->isInt2 = true;
-					tac->l1 = s1->place;
-					tac->l2 = s2->place;
-					code.pb(tac);
-					return;
-				}
-				else
-					printf("Error: Incompatible operands to operator %s near line %d", op, lineNum);
+				tac->isInt1 = true;
+				tac->isInt2 = true;
+				tac->l1 = s1->place;
+				tac->l2 = s2->place;
 			}
 			else if(s1->isLit && !s2->isLit){
-				if(s1->type == "int"){
-					if(s2->type=="int"){
-						
-					}					
+				tac->isInt1 = true;
+				tac->l1 = s1->place;
+				Symbol* sym2 = ST.get(s2->place);
+				if(sym2==NULL){
+					printf("Error: Symbol %s not defined in scope.", s2->place)
+					exit();
 				}
+				tac->opd2 = ST.get(sym2);	
 			}
-			Symbol* sym1 = ST.get(s1->place);
-			Symbol* sym2 = ST.get(s2->place);
-			if(sym1==NULL){
-				printf("Error: Symbol %s not defined in scope.", s1)
-			} else if(sym2==NULL){
-				printf("Error: Symbol %s not defined in scope.", s2)
+			else if(!s1->isLit && s2->isLit){
+				tac->isInt2 = true;
+				tac->l2 = s2->place;
+				Symbol* sym1 = ST.get(s1->place);
+				if(sym1==NULL){
+					printf("Error: Symbol %s not defined in scope.", s1->place)
+					exit();
+				}
+				tac->opd1 = ST.get(sym1);	
+			}	
+			else{
+				Symbol* sym1 = ST.get(s1->place);
+				if(sym1==NULL){
+					printf("Error: Symbol %s not defined in scope.", s1->place)
+					exit();
+				}
+				tac->opd1 = ST.get(sym1);	
+				Symbol* sym2 = ST.get(s2->place);
+				if(sym2==NULL){
+					printf("Error: Symbol %s not defined in scope.", s2->place)
+					exit();
+				}
+				tac->opd2 = ST.get(sym2);	
+			}			
+
+			// Type-Checking
+			if (s1->type = "char"){
+				if(s2->type=="int")		temp->type="int";
+				else if(s2->type == "long")		temp->type=="long";
+				else{
+					printf("Error: Incompatible operands to operator %s near line %d", op, lineNum);
+					exit();
+				}
+				code.pb(tac);
 			}
-			if((sym1->type=="int" || sym1->type=="long" || sym1->type=="char" || sym1->type=="uint" || sym1->type=="ulong") &&
-				(sym2->type=="int" || sym2->type=="long" || sym2->type=="char" || sym2->type=="uint" || sym2->type=="ulong") ){
-				
-				
-				
-			}
+			else if(s1->type == "int"){
+				if(s2->type=="char" || s2->type=="int")		temp->type="int";
+				else if(s2->type=="long")		temp->type="long";
+				else{
+					
+				}
+				code.pb(tac);
+			}	
+			else if(s1->type == "long"){
+				if(s2->type=="char" || s2->type=="int" || s2->type=="long")	temp->type="long";
+				else{
+					printf("Error: Incompatible operands to operator %s near line %d", op, lineNum);
+					exit();
+				}
+				code.pb(tac);
+			}			
 		}
+		return;
 	}
 }
