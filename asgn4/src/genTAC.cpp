623b3cc8e78181040effc1f5d309ce95dbcf5340
Amrit Singhal <amritsinghal97@gmail.com>
2018-04-02 00:14:41 +0530

    two functions done in genTAC

--- asgn4/src/genTAC.cpp
+++ asgn4/src/genTAC.cpp
@@ -101,10 +101,72 @@ void gen2OpCode(genNode* d, string op, genNode* s1, genNode* s2, int lineNum){
 	return;
 }
 
+string equal_compatible(string s1, string s2){
+	if(s1=="char"){
+		if(s2=="char")	return "char";
+		else if(s2=="int") return "int";
+		else if(s2=="long") return "long";
+		else return NULL;
+	}
+	else if(s1=="int"){
+		if (s2=="char" || s2=="int") return "int";
+		else if(s2=="long") return "long";
+		else return NULL;
+	}
+	else if(s1=="long"){
+		if(s2=="char"||s2=="int"||s2=="long") return "long";
+		else return NULL;
+	}
+	else if(s1==s2)	return s1;
+	else return NULL;
+}
+
 void getCECode(genNode* d, genNode* c, genNode* s1, genNode* s2, int lineNum){
 	d->code = c->code;
-	TAC* tac = new TAC();
-	string newLabel1 = new getNewLabel();
+	Symbol* temp = getTemp();
+	d->place = temp->name;
+	d->type = equal_compatible(s1->type, s2->type);
+	if(d->type==NULL){
+		printf("Error: Incompatible types to conditional expression near line %d", op, lineNum);
+		exit();
+	}
+	TAC* tac1 = new TAC();	TAC* tac2 = new TAC();
+	TAC* tac3 = new TAC();	TAC* tac4 = new TAC();
+	TAC* tac5 = new TAC();	TAC* tac6 = new TAC();
+	TAC* tac7 = new TAC(); 	TAC* tac8 = new TAC();
+	// getNewLabel() has to generate a new label name, create a symbol with that name, and type "label" and add it to the symbol table, and return 
+	// the name of this new symbol, as a string. 
+	string newLabel1 = new getNewLabel();			
 	string newLabel2 = new getNewLabel();
-	
+	string newLabel3 = new getNewLabel();
+	tac1->op = "label"; tac1->target = newLabel1;
+	tac2->op = "label"; tac2->target = newLabel2;
+	tac3->op = "ifgoto"; tac3->dest = ST.get(c->place); tac3->target = newLabel1;
+	tac4->op = "goto"; tac4->target = newLabel2;
+	tac5->op = "goto"; tac5->target = newLabel3;
+	tac6->op = "label"; tac6->target = newLabel3;
+	tac7->op = "="; tac7->dest=temp;
+	if(s1->isLit){
+		tac7->IsInt1=true;
+		tac7->l1=s1->place;
+	}
+	else 	tac7->opd1 = ST.get(s1->place);
+
+	tac8->op = "="; tac8->dest=temp;
+	if(s2->isLit){
+		tac8->IsInt1=true;
+		tac8->l1=s2->place;
+	}
+	else 	tac8->opd1 = ST.get(s2->place);
+
+	d->code.pb(tac3); d->code.pb(tac4);
+	d->code.pb(tac1);
+	d->code.insert(d->code.end(), s1->code.start(), s1->code.end());
+	d->code.pb(tac7);
+	d->code.pb(tac5);
+	d->code.pb(tac2);
+	d->code.insert(d->code.end(), s1->code.start(), s1->code.end());
+	d->code.pb(tac8);
+	d->code.pb(tac6);
+	return;
 }
