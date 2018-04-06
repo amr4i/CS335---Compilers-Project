72716a6cf79673083b71d94cb033af54c29f6c5e
Shobhit Rastogi <shobhitr97@gmail.com>
2018-04-06 23:03:26 +0530

    test0.cs

--- asgn4/test/test0.cs
+++ asgn4/test/test0.cs
@@ -3,18 +3,14 @@ namespace Rextester
 {
     public class Program
     {
+        public int fn(int x){
+            return x;
+        }
+
         public void Main(string[] args)
         {
-		int  a = 5;
-		while(a<=10){
-			a = a + 1;
-			if(a != 1){
-				int p = 6;
-				continue;
-			}
-		}
-		
-		int b = a;
+            fn(2);
+		    int  a = 5;
         }
     }
 }
