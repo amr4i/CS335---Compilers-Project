6be64a57b828d3aed3043a754034d01d6926ac76
Shobhit Rastogi <shobhitr97@gmail.com>
2018-04-13 22:37:13 +0530

    no changes

--- asgn4/test/test0.cs
+++ asgn4/test/test0.cs
@@ -4,8 +4,13 @@ namespace Rextester
     public class Program
     {
         public int fn(int x, int y, int c){
-	    c = x + y;
+	           c = x + y;
             return c;
+        }
+
+        public int pp(int c){
+            int x = c;
+            return x;
         }
 
         public void Main(string[] args)
@@ -15,6 +20,7 @@ namespace Rextester
             if(c[1] > 20){
                 long m = 3;
             }
+            pp(c[0]);
             int k = 10;  
             int t = fn(9, 3, 4);
         }
